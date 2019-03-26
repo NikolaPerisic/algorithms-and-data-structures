a2550bad5ae6ad619f2dca84372ed264977e32ad
Nikola Perisic <cathode.y2k@gmail.com>
2019-03-27 00:13:41 +0100

    bubble sort

---
+++ sorting_algorithms/bubble-sort.js
@@ -0,0 +1,22 @@
+/*
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+*/
+
+function countSwaps(a) {
+    let swaps = 0;
+    let arr = [...a];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swaps++;
+            }
+        }
+    }
+    return `Array is sorted in ${swaps} swaps. \nFirst Element: ${
+        arr[0]
+    }.\nLast Element: ${arr[arr.length - 1]} \nOutput: ${arr}`;
+}
+console.log(countSwaps([3, 4, 5, 1, 2, 7, 9, 8, 6]));
