bba4ae704f032e5c68af6821ae17bcde31424b0e
Nikola Perisic <cathode.y2k@gmail.com>
2019-04-07 20:26:28 +0200

    stack

--- data_structures/stack.js
+++ data_structures/stack.js
@@ -9,17 +9,31 @@ class Stack {
     constructor() {
         this._top = null;
         this.length = 0;
+        this.min = [];
+        this.index = this.min.length;
     }
     push(item) {
         const node = new Node(item);
         node.next = this._top;
         this._top = node;
         this.length++;
+        if (this.min.length === 0) {
+            this.min.push(this._top.data);
+            this.index++;
+        }
+        if (this._top.data < this.min[this.index - 1]) {
+            this.min.push(this._top.data);
+            this.index++;
+        }
         return this.length;
     }
     pop() {
         if (this.isEmpty()) throw new Error("Empty Stack");
         const node = this._top;
+        if (node.data === this.min[this.index - 1]) {
+            this.min.pop();
+            this.index--;
+        }
         this._top = this._top.next;
         node.next = null;
         this.length--;
@@ -41,17 +55,19 @@ class Stack {
         }
         return output;
     }
+    minVal() {
+        return this.min[this.index - 1];
+    }
 }
 
 let stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-stack.push(4);
 stack.push(5);
-console.log(stack);
+stack.push(6);
+stack.push(3);
+stack.push(7);
+console.log(stack.minVal());
 console.log(stack.enumerate());
-console.log(stack.pop());
+stack.pop();
+stack.pop();
+console.log(stack.minVal());
 console.log(stack.enumerate());
-console.log(stack.isEmpty());
-console.log(stack.peek());
